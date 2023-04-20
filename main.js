0232020b0a91ebedba028086f64de9adf34c4002
Elisafa <elisafajunior@hotmail.com>
2023-04-20 15:33:28 -0300

    Alterações para tratamento de um possível error

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-$(document).ready(function (){
-    const endpoint = `https://api.github.com/users/elisafa`
-    
-    fetch(endpoint)
-    .then(function(resposta){
-        return resposta.json()
+$(document).ready(function () {
+  const endpoint = `https://api.github.com/users/elisafa`;
+  fetch(endpoint)
+    .then(function (resposta) {
+      return resposta.json();
     })
-    .then(function(json){
-        console.log(json)
-        const avatar = json.avatar_url
-        $('.profile-avatar').attr("src", avatar);
-        
-        
-        const nome = json.login
-        $('.profile-username').html(`@${nome}`)
+    .then(function (json) {
+      const avatar = json.avatar_url;
+      const nome = json.login;
+      const repositorio = json.public_repos;
+      const seguidores = json.followers;
+      const seguindo = json.following;
+      const link = json.html_url;
 
-        const repositorio = json.public_repos
-        $('#repositorio').html(`${repositorio}`)
-
-        const seguidores = json.followers
-        $('#followers').html(seguidores)
-
-        const seguindo = json.following
-        $('#following').html(seguindo)
-
-        const link = json.html_url
-        console.log(link)
-        $('.profile-link').attr("href", link)
-    })
-})
+      if (avatar || nome || repositorio || seguidores || seguindo || link) {
+        $(".profile-avatar").attr("src", avatar);
+        $(".profile-username").html(`@${nome}`);
+        $("#repositorio").html(`${repositorio}`);
+        $("#followers").html(seguidores);
+        $("#following").html(seguindo);
+        $(".profile-link").attr("href", link);
+      } else {
+        alert("Ocorreu um erro ao buscar suas informações, tente novamente mais tarde!");
+      }
+    });
+});
